fb6dbe27a15deb3c941437c3e05e73c49b55f85d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-02 23:20:02 +0300

    Update phonia.py

--- phonia.py
+++ phonia.py
@@ -43,7 +43,7 @@ from scanners import recon
 
 def scanNumber(InputNumber):
     os.system("clear")
-    os.system("cat ~/phonia/banner/banner.txt")
+    os.system("cat banner/banner.txt")
     print("")
     time.sleep(3)
     title("[!] ---- Fetching informations for {} ---- [!]".format(formatNumber(InputNumber)))
@@ -67,7 +67,7 @@ def main():
     scanners = ['any', 'all', 'numverify', 'ovh', 'footprints']
     
     if args.update:
-        os.system("cd ~/phonia && chmod +x bin/phonia && bin/phonia -u")
+        os.system("chmod +x bin/phonia && bin/phonia -u")
         sys.exit()
 
     banner()
@@ -83,7 +83,7 @@ def main():
         parser.print_help()
         sys.exit()
     elif args.info:
-        os.system("cat ~/phonia/banner/banner1.txt")
+        os.system("cat banner/banner1.txt")
         sys.exit()
 
     if args.output:
