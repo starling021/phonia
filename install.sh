a475c54563f553ff95af803050829af16d9b1ea0
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-16 09:44:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ fi
 
 else
 
-read -p $'Select your architecture (amd/intel/arm) ' CONF
+read -p $'Select your architecture (amd/intel/arm): ' CONF
 if [[ "$CONF" = "" ]]
 then
 exit
@@ -227,7 +227,7 @@ fi
 
 else
 
-read -p $'Select your architecture (amd/intel/arm) ' CONF
+read -p $'Select your architecture (amd/intel/arm): ' CONF
 if [[ "$CONF" = "" ]]
 then
 exit
