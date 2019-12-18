d961c488e4e1ae52a698ae8254462209170e3c2c
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-18 21:38:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -67,35 +67,44 @@ sleep 1
 pkg update
 pkg -y install python
 pkg -y install wget
+apt-get update
+apt-get -y install git
 apt-get -y install python3
 apt-get -y install python3-pip
 apt-get -y install wget
 apk update
+apk add pip
 apk add python3
 apk add py3-pip
 apk add wget
 pacman -Sy
+pacman -S --noconfirm git
 pacman -S --noconfirm python3
 pacman -S --noconfirm python3-pip
 pacman -S --noconfirm wget
 zypper refresh
+zypper install -y git
 zypper install -y python3
 zypper install -y python3-pip
 zypper install -y wget
+yum -y install git
 yum -y install python3
 yum -y install python3-pip
 yum -y install wget
+dnf -y install git
 dnf -y install python3
 dnf -y install python3-pip
 dnf -y install wget
 eopkg update-repo
+eopkg -y install git
 eopkg -y install python3
 eopkg -y install pip
 eopkg -y install wget
 xbps-install -S
+xbps-install -y git
 xbps-install -y python3
 xbps-install -y python3-pip
-xbps-install wget
+xbps-install -y wget
 } &> /dev/null
 
 {
