d0a3ac627833b43d75e5af9faf02ed3927397abc
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-04 23:03:35 +0300

    Update args.py

--- lib/args.py
+++ lib/args.py
@@ -5,13 +5,13 @@ from lib.banner import __version__
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-n', '--number', dest='number', help='The phone number to scan.')
+parser.add_argument('-n', '--number', dest='number', type=str, help='The phone number to scan.')
 
-parser.add_argument('-i', '--input', dest="inputfile", help='List of phone numbers to scan.')
+parser.add_argument('-i', '--input', dest="inputfile", type=str, help='List of phone numbers to scan.')
 
-parser.add_argument('-o', '--output', dest="outputfile", help='Output to save scan results.')
+parser.add_argument('-o', '--output', dest="outputfile", type=str, help='Output to save scan results.')
 
-parser.add_argument('-s', '--scanner', dest="scanner", default="all", help='The scanner to use.')
+parser.add_argument('-s', '--scanner', dest="scanner", type=str, default="all", help='The scanner to use.')
 
 parser.add_argument('--recon', action='store_true', help='Launch custom format reconnaissance.')
 
