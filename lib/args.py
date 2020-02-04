dcc2ccaf532c23de69004da09cef4a3c27fab744
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-04 22:42:33 +0300

    Update args.py

--- lib/args.py
+++ lib/args.py
@@ -3,15 +3,15 @@
 import argparse
 from lib.banner import __version__
 
-parser = argparse.ArgumentParser(usage='%(prog)s -n <number> [options]')
+parser = argparse.ArgumentParser()
 
-parser.add_argument('-n', '--number', metavar='number', type=str, help='The phone number to scan.')
+parser.add_argument('-n', '--number', dest='number', help='The phone number to scan.')
 
-parser.add_argument('-i', '--input', metavar="input_file", type=str, help='Phone number list to scan.')
+parser.add_argument('-i', '--input', dest="inputfile", help='List of phone numbers to scan.')
 
-parser.add_argument('-o', '--output', metavar="output_file", type=str, help='Output to save scan results.')
+parser.add_argument('-o', '--output', dest="outputfile", help='Output to save scan results.')
 
-parser.add_argument('-s', '--scanner', metavar="scanner", default="all", type=str, help='The scanner to use.')
+parser.add_argument('-s', '--scanner', dest="scanner", default="all", help='The scanner to use.')
 
 parser.add_argument('--recon', action='store_true', help='Launch custom format reconnaissance.')
 
