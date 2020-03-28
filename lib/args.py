1d0b0592f61cdb432a8482c5a8d3bb9a71594038
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-29 01:41:30 +0300

    Update args.py

--- lib/args.py
+++ lib/args.py
@@ -24,22 +24,14 @@
 
 import argparse
 import os
-from lib.banner import __version__
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument('-p', '--phone', metavar="<phone>", dest='phone', type=str, help='The phone number to scan.')
-
 parser.add_argument('-i', '--input', metavar="<inputfile>", dest="inputfile", type=str, help='List of phone numbers to scan.')
-
 parser.add_argument('-o', '--output', metavar="<outputfile>", dest="outputfile", type=str, help='Output to save scan results.')
-
 parser.add_argument('-s', '--scanner', metavar="<scanner>", dest="scanner", type=str, default="all", help='The scanner to use.')
-
 parser.add_argument('--recon', action='store_true', help='Launch custom format reconnaissance.')
-
 parser.add_argument('--no-ansi', action='store_true', help='Disable colored output.')
-
 parser.add_argument('-u', '--update', action='store_true', help='Update Phonia Toolkit.')
 
 args = parser.parse_args()
