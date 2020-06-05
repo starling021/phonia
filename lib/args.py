0e1ee442a3134648ef72941c2b5958dd3cf7a541
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-05 12:54:04 +0300

    Update args.py

--- lib/args.py
+++ lib/args.py
@@ -27,8 +27,8 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--phone', metavar="<phone>", dest='phone', type=str, help='The phone number to scan.')
-parser.add_argument('-i', '--input', metavar="<input_file>", dest="inputfile", type=str, help='List of phone numbers to scan.')
-parser.add_argument('-o', '--output', metavar="<output_path>", dest="outputfile", type=str, help='Output to save scan results.')
+parser.add_argument('-i', '--input', metavar="<input_file>", dest="inputfile", type=str, help='List of phone numbers.')
+parser.add_argument('-o', '--output', metavar="<output_path>", dest="outputfile", type=str, help='Output to the specified path.')
 parser.add_argument('-s', '--scanner', metavar="<scanner>", dest="scanner", type=str, default="all", help='The scanner to use.')
 parser.add_argument('--recon', action='store_true', help='Launch custom format reconnaissance.')
 parser.add_argument('--no-ansi', action='store_true', help='Disable colored output.')
